ab5db7476cc578d506a293f851b9f6e250f54be0
Bharath Kumar Ravichandran <bharathkumarravichandran@gmail.com>
2019-06-21 07:19:07 +0530

    Feat: Add helper class for handling single and multiple file uploads

---
+++ src/utils/upload.util.js
@@ -0,0 +1,91 @@
+const formidable = require('formidable');
+const path = require('path');
+const multer = require('multer');
+const util = require('util');
+
+// Importing config/env variables
+const config = require('../config/config');
+
+
+class Uploader {
+
+    constructor(req,applicationNumber,fileOptions) {
+
+        // Set The Storage Engine
+        const storageOptions = multer.diskStorage({
+            destination: path.join(config.directory.UPLOADS_DIR,applicationNumber),
+            filename: function (req, file, cb) {
+                cb(null, fileOptions.fileName + path.extname(file.originalname));
+            },
+            onError : function(error, next) {
+                return {
+                    status_code: 400,
+                    message: error.toString()
+                }
+            }
+        });
+
+        // Check File Type
+        const checkFileType = (file, allowedFileTypesRE, cb) => {
+            // Check extension
+            const extname = allowedFileTypesRE.test(path.extname(file.originalname).toLowerCase());
+            // Check mimetype
+            const mimetype = allowedFileTypesRE.test(file.mimetype);
+            if (mimetype && extname) {
+                return cb(null, true);
+            } else {
+                cb('Error: Upload only allowed file types');
+            }
+        }
+
+        this.upload = multer({
+            storage: storageOptions,
+            limits: { fileSize: fileOptions.allowedFileSize },
+            fileFilter: function (req, file, cb) {   
+                checkFileType(file,fileOptions.allowedFileTypesRE,cb);
+            }
+        });
+
+    }
+
+    async uploadSingle(req, res, fieldName) {
+        try {
+            const upload = util.promisify(this.upload.single(fieldName));
+            let response = await upload(req, res);
+            return {
+                status_code: 200,
+                message: 'File has been successfully uploaded.',
+                data: {
+                    file: req.file
+                }
+            }
+        } catch (error) {
+            return {
+                status_code: 400,
+                message: error.toString()
+            }
+        }
+    }
+
+    async uploadMulti(req, res, fieldDetails) {
+        try {
+            const upload = util.promisify(this.upload.array(fieldDetails.fieldName,fieldDetails.maxCount));
+            let response = await upload(req, res);
+            return {
+                status_code: 200,
+                message: 'Files have been successfully uploaded.',
+                data: {
+                    files: req.files
+                }
+            }
+        } catch (error) {
+            return {
+                status_code: 400,
+                message: error.toString()
+            }
+        }
+    }
+
+}
+
+module.exports = Uploader;
