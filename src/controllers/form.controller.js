e62c4fb40eb6e2398d936fca453c68ab351dd8f7
Bharath Kumar Ravichandran <bharathkumarravichandran@gmail.com>
2019-07-16 00:19:43 +0530

    Feat: Add routes to get form access and application number

--- src/controllers/form.controller.js
+++ src/controllers/form.controller.js
@@ -15,7 +15,60 @@ const formUtil = require('../utils/form.util');
 const pdfUtil = require('../utils/pdf.util');
 
 
-exports.viewApplication = async (req, res, next) => {
+exports.formAccess = async (req, res) => {
+    try {
+        const email = req.session.student.email;
+        let student = await Student.findOne({email: email}).exec();
+
+        let access = false;
+        if(student.isVerified1 && student.isVerified2){
+            access = true;
+        }
+
+        logger.info(`Form access for ${email}: ${access}`);
+        return res.status(200).json({
+            status_code: 200,
+            message: `Success`,
+            data: {
+                access: access
+            }
+        });
+    } catch(error){
+        logger.error(error.toString());
+        return res.status(400).json({
+            status_code: 400,
+            message: error.toString(),
+            data: {}
+        });
+    }
+}
+
+exports.getApplicationNumber = async (req, res) => {
+    try {
+        const email = req.session.student.email;
+        let student = await Student.findOne({email: email}).exec();
+
+        let applicationNumber = student.applicationNumber;
+
+        logger.info(`Application Number for ${email}: ${applicationNumber}`);
+        return res.status(200).json({
+            status_code: 200,
+            message: `Success`,
+            data: {
+                applicationNumber: applicationNumber
+            }
+        });
+    } catch(error){
+        logger.error(error.toString());
+        return res.status(400).json({
+            status_code: 400,
+            message: error.toString(),
+            data: {}
+        });
+    }
+}
+
+exports.viewApplication = async (req, res) => {
     try{
         const email = req.session.student.email;
         let student = await Student.findOne({email: email}).exec();
@@ -96,7 +149,7 @@ exports.viewApplication = async (req, res, next) => {
     }
 }
 
-exports.submitForm = async (req, res, next) => {
+exports.submitForm = async (req, res) => {
     try{
         const email = req.session.student.email;
         let student = await Student.findOne({email: email}).exec();
--- src/routes/form.router.js
+++ src/routes/form.router.js
@@ -3,8 +3,16 @@ const router = require('express').Router();
 // Importing controllers
 const formController = require('../controllers/form.controller');
 
+// Importing middlewares
+const authMiddleware = require('../middlewares/auth/student.middleware');
+const checkAccountActivation = authMiddleware.checkAccountActivation;
+
+// Form access and get routes
+router.get('/access', formController.formAccess);
+router.get('/application-number', formController.getApplicationNumber);
+
 // Final form submission routes
-router.post('/view', formController.viewApplication);
-router.post('/submit', formController.submitForm);
+router.post('/view', checkAccountActivation, formController.viewApplication);
+router.post('/submit', checkAccountActivation, formController.submitForm);
 
 module.exports = router;
